50b0552d9876cb10f0940869b7f309921520713c
Matheus Carvalho Hatzlhoffer <matheus.a.carvalho@ericsson.com>
2024-06-11 17:16:39 -0300

    Project course started with basic components and styling

---
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Delicious Recipe',
+      'A recipe that is delicious',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'
+    ),
+  ];
+}
