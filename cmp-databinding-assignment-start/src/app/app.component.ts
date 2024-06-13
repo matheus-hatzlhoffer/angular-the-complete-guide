22a9cf506d017e5b2a283fa488136745e2324fd7
Matheus Carvalho Hatzlhoffer <matheus.a.carvalho@ericsson.com>
2024-06-13 19:19:27 -0300

    fourth assignment done - Module 9

--- cmp-databinding-assignment-start/src/app/app.component.ts
+++ cmp-databinding-assignment-start/src/app/app.component.ts
@@ -3,7 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  numbers: number[] = [];
+
+  onCountEvent(number: number) {
+    console.log(number);
+    this.numbers.push(number);
+  }
+
+  onClearEvent() {
+    console.log(this.numbers);
+    console.log('clear numbers');
+    this.numbers = [];
+  }
 }
---
+++ cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrl: './game-control.component.css',
+})
+export class GameControlComponent {
+  count = 0;
+  intervalCount: NodeJS.Timeout;
+  @Output('count') countEvent = new EventEmitter<number>();
+  @Output('clear') clearCountEvent = new EventEmitter();
+
+  startGame() {
+    if (this.intervalCount === undefined) {
+      this.intervalCount = setInterval(this.emitEvent, 1000);
+    }
+  }
+
+  stopGame() {
+    clearInterval(this.intervalCount);
+    this.count = 0;
+    this.intervalCount = undefined;
+    this.clearCountEvent.emit();
+  }
+
+  private emitEvent = () => {
+    this.countEvent.emit(this.count);
+    console.log(this.count);
+    this.count++;
+  };
+}
